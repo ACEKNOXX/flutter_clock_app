4c23edd27ae19ea27c67e9185804c4abddba8a78
Aceknoxx <mustapha.ddare@gmail.com>
2020-08-04 22:58:20 +0100

    updated the ui intensity and also made the clock ui reactive in reatime

--- lib/screens/clockView.dart
+++ lib/screens/clockView.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -9,19 +10,34 @@ class ClockView extends StatefulWidget {
 
 class _ClockViewState extends State<ClockView> {
   @override
+  void initState() {
+    Timer.periodic(Duration(seconds: 1),(timer){
+      setState(() {
+        
+      });
+    });
+    super.initState();
+   
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       width: 300.0,
       height: 300.0,
       // color: Colors.redAccent,
-      child: CustomPaint(
-        painter: ClockPainter(),
+      child: Transform.rotate(
+        angle: -pi / 2,
+        child: CustomPaint(
+          painter: ClockPainter(),
+        ),
       ),
     );
   }
 }
 
 class ClockPainter extends CustomPainter {
+  var dateTime = DateTime.now();
   @override
   void paint(Canvas canvas, Size size) {
     var centerX = size.width / 2;
@@ -34,7 +50,7 @@ class ClockPainter extends CustomPainter {
     var outlineBrush = Paint()
       ..color = Color(0xFFEAECFF)
       ..style = PaintingStyle.stroke
-      ..strokeWidth = 16;
+      ..strokeWidth = 1;
 
     var centerFillBrush = Paint()..color = Color(0xFFEAECFF);
 
@@ -42,28 +58,40 @@ class ClockPainter extends CustomPainter {
       ..color = Colors.orange[300]
       ..style = PaintingStyle.stroke
       ..strokeCap = StrokeCap.round
-      ..strokeWidth = 8;
+      ..strokeWidth = 2;
 
     var minHandBrush = Paint()
       ..shader = RadialGradient(colors: [Color(0xFF758EF6), Color(0xFF77DFF)])
           .createShader(Rect.fromCircle(center: center, radius: radius))
       ..style = PaintingStyle.stroke
       ..strokeCap = StrokeCap.round
-      ..strokeWidth = 16;
+      ..strokeWidth = 8;
 
     var hourHandBrush = Paint()
       ..shader = RadialGradient(colors: [Color(0xFFEA74AB), Color(0xFFC279FB)])
           .createShader(Rect.fromCircle(center: center, radius: radius))
       ..style = PaintingStyle.stroke
       ..strokeCap = StrokeCap.round
-      ..strokeWidth = 16;
+      ..strokeWidth = 8;
 
     canvas.drawCircle(center, radius - 40, fillBrush);
     canvas.drawCircle(center, radius - 40, outlineBrush);
-    canvas.drawCircle(center, 16, centerFillBrush);
-    canvas.drawLine(center, Offset(100, 100), secHandBrush);
-    canvas.drawLine(center, Offset(150, 100), minHandBrush);
-    canvas.drawLine(center, Offset(200, 100), hourHandBrush);
+
+    var hourHandx = centerX +
+        80 * cos((dateTime.hour * 30 + dateTime.minute * 0.5) * pi / 180);
+    var hourHandY = centerX +
+        80 * sin((dateTime.hour * 30 + dateTime.minute * 0.5) * pi / 180);
+    canvas.drawLine(center, Offset(hourHandx, hourHandY), hourHandBrush);
+
+    var minHandX = centerX + 80 * cos(dateTime.minute * 6 * pi / 180);
+    var minHandY = centerX + 80 * sin(dateTime.minute * 6 * pi / 180);
+    canvas.drawLine(center, Offset(minHandX, minHandY), minHandBrush);
+
+    var secHandX = centerX + 80 * cos(dateTime.second * 6 * pi / 180);
+    var secHandY = centerX + 80 * sin(dateTime.second * 6 * pi / 180);
+    canvas.drawLine(center, Offset(secHandX, secHandY), secHandBrush);
+
+    canvas.drawCircle(center, 8, centerFillBrush);
   }
 
   @override
